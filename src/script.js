63adc590a0da7c59a9855d3187d04a10403baf21
Jos <joheandroid@gmail.com>
2021-07-07 15:56:46 -0400

    Fix

--- src/script.js
+++ src/script.js
@@ -170,7 +170,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.setClearColor(new THREE.Color('#21282a'), 1)
+renderer.setClearColor(new THREE.Color('#000'), 1)
 // MOUSE
 /**
  * Animate
