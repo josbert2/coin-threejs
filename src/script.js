5aad4747db3f72f9bc8a0f9e3cd5ea1abb481608
jos <joheandroid@gmail.com>
2021-07-07 23:46:17 -0400

    Fix

--- src/script.js
+++ src/script.js
@@ -42,7 +42,7 @@ particlesGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3
 
 
 const material = new THREE.PointsMaterial({
-    size: 0.001,
+    size: 0.009,
     transparent: true,
     alphaTest: 0.9,
     opacity: 0.9,
@@ -75,49 +75,6 @@ scene.add(sphere, particlesMesh)
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 6
-pointLight.position.z = 4
-scene.add(pointLight)
-// LIGHT 2
-const pointLight2 = new THREE.PointLight(0xff0000, 2)
-pointLight2.position.set(1,1,1)
-pointLight2.intensity = 0.21
-scene.add(pointLight2)
-
-const light1 = gui.addFolder('Light 1') 
-
-light1.add(pointLight2.position, 'y').min(-3).max(3).step(0.01)
-light1.add(pointLight2.position, 'x').min(-3).max(6).step(0.01)
-light1.add(pointLight2.position, 'z').min(-3).max(3).step(0.01)
-light1.add(pointLight2, 'intensity').min(0).max(10).step(0.01)
-
-/*const pointLightHelper = new THREE.PointLightHelper(pointLight2, 2)
-scene.add(pointLightHelper) */
-
-
-// LIGHT 3
-const light2= gui.addFolder('Light 2') 
-
-const pointLight3 = new THREE.PointLight(0x4a00ff, 2)
-pointLight3.position.set(-2.31,-1.27,-1.78)
-pointLight3.intensity = 0.49
-scene.add(pointLight3)
-
-
-light2.add(pointLight3.position, 'y').min(-3).max(3).step(0.01)
-light2.add(pointLight3.position, 'x').min(-3).max(6).step(0.01)
-light2.add(pointLight3.position, 'z').min(-3).max(3).step(0.01)
-light2.add(pointLight3, 'intensity').min(0).max(10).step(0.01)
-
-const light2Color = {
-    color: 0xff0000
-}
-light2.addColor(light2Color, 'color')
-      .onChange(() => {
-    pointLight3.color.set(light2Color.color)
-})
 /*const pointLightHelper2 = new THREE.PointLightHelper(pointLight3, 2)
 scene.add(pointLightHelper2) */
 
